e740684cb98466210150facb7ac24749ab8e47b8
Niraj-123456 <lamaniraj321@gmail.com>
2021-05-13 17:33:20 +0545

    Add: Active links

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 var date = new Date().getFullYear();
 document.getElementById("Date").innerHTML = date;
+
+var current = location.pathname;
+
+$(function(){
+    var current = location.pathname;
+    $('.Link').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if ($this.attr('href') === current) {
+            $('.Link').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+});
+
+$('.Search').click(function(e){
+    e.preventDefault();
+    $('.Link').hide();
+    $('.SearchClick', 'input').show();
+})
+
+// $(document).ready(function(){
+//     $('.SearchClick').hide();
+// })
